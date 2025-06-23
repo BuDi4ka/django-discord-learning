3ac0545fdab434ec84510bebc5e1cfb064b5afd5
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-23 09:08:52 +0100

    first commit

---
+++ base/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+
+def room(request):
+    return HttpResponse('Room') 
