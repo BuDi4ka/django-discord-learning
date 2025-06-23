37fb814cf3accf52c2ae1c551a6e8d7ff4989041
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-23 13:05:17 +0100

    login users

--- base/views.py
+++ base/views.py
@@ -1,15 +1,33 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 from .models import Room, Topic
 from .forms import RoomForm
 
-# rooms = [
-#     {"id": 1, "name": "Lets learn python!"},
-#     {"id": 2, "name": "Design with me"},
-#     {"id": 3, "name": "Frontend developers"},
-# ]
 
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('base:home')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') else ''
