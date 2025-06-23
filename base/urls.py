cd3f939452027772ae1e286cd0fe83ddc823e76c
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-23 11:40:52 +0100

    create and update room

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,7 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/<str:pk>/', views.room, name="room")
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.create_room, name="create-room"),
+    path('update-room/<str:pk>', views.update_room, name="update-room"),
 ]
