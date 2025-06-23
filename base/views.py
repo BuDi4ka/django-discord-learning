59c4d2e20c046b2ce755a1479680bc726caeb641
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-23 13:10:26 +0100

    logout users

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
@@ -28,6 +28,11 @@ def login_page(request):
 
     context = {}
     return render(request, 'base/login_register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('base:home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') else ''
