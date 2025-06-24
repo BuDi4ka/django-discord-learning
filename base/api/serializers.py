6631cd34b012546cd4c0623bce8ab4bd092c8962
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-24 13:21:38 +0100

    simple api with drf

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,1 +1,18 @@
+from rest_framework import serializers
 
+from base.models import Room
+
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    host = serializers.CharField(source='host.username', read_only=True)
+    topic = serializers.CharField(source='topic.name', read_only=True)
+    participants = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Room
+        fields = '__all__'
--- base/api/urls.py
+++ base/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_routes),
+    path("", views.get_routes),
+    path("rooms/", views.get_rooms),
+    path("rooms/<str:pk>", views.get_room),
 ]
