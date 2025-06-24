a881a3ccace0a6702adc79cc73bbae5e3526e9fc
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-24 12:28:19 +0100

    mobile optimization (topics, activity)

--- base/views.py
+++ base/views.py
@@ -82,7 +82,7 @@ def home(request):
         Q(topic__name__contains=q) | Q(name__icontains=q) | Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -200,3 +200,17 @@ def update_user(request):
 
     context = {"form": form}
     return render(request, 'base/update-user.html', context)
+
+
+def topics_page(request):
+    q = request.GET.get("q") if request.GET.get("q") else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {"topics": topics}
+    return render(request, 'base/topics.html', context)
+
+
+def activity_page(request):
+    room_messages = Message.objects.all()
+    context = {"room_messages": room_messages}
+    return render(request, 'base/activity.html', context)
+
